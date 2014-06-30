66d7bb1c85f42013483216d896e1b0e6389c7538
jsdevel <js.developer.undefined@gmail.com>
2014-06-30 12:24:17 -0700

    Initial commit

---
+++ test/is-file.js
@@ -0,0 +1,42 @@
+'use strict';
+
+describe('is-file', function(){
+  var sut = require('..');
+
+  describe('async', function(){
+    it('should send back an error when the path does not exist', function(done){
+      sut('asdasdf', function(err, bool){
+        err.should.be.an.instanceOf(Error);
+        done();
+      });
+    });
+
+    it('should send back false for directories', function(done){
+      sut(__dirname, function(err, bool){
+        bool.should.be.false;
+        done();
+      });
+    });
+
+    it('should send back true for files', function(done){
+      sut(__filename, function(err, bool){
+        bool.should.be.true;
+        done();
+      });
+    });
+  });
+
+  describe('sync', function(){
+    it('may be invoked with .sync()', function(){
+      sut.sync(__filename).should.be.true;
+    });
+
+    it('should return false for directories', function(){
+      sut(__dirname).should.be.false;
+    });
+
+    it('should return true for files', function(){
+      sut(__filename).should.be.true;
+    });
+  });
+});
